d5908923169e16ceda56c3bf345b496fd806fe88
Elijah <elijahmtcastle@gmail.com>
2023-04-10 11:27:44 -0600

    now a nice class, need to fix up the seeding being randomized

--- perlin.py
+++ perlin.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python3
 import pyfastnoisesimd as fns
 import matplotlib.pyplot as plt#just for displaying the image
-import time
 import numpy as np
-tmp   = 1028
-shape = [tmp,tmp]
-seed  = np.random.randint(2**31)
-N_threads = 4
+class Perlin():
+    shape = [1028,1028]
+    seed  = np.random.randint(2**31)
+    N_threads = 4#TODO this can be changed to match how mnay cores your cpu has
+    def __init__(self):
+        self.perlin = fns.Noise(seed=self.seed, numWorkers=self.N_threads)
+        self.perlin.frequency = 0.005
+        self.perlin.noiseType = fns.NoiseType.Perlin
+        self.perlin.fractal.octaves = 8
+        self.perlin.fractal.lacunarity = 1.0#does not seem to be implemented
+        self.perlin.perturb.perturbType = fns.PerturbType.GradientFractal
+        self.randomVals()
 
-perlin = fns.Noise(seed=seed, numWorkers=N_threads)
-perlin.frequency = 0.02
-perlin.noiseType = fns.NoiseType.Perlin
-perlin.fractal.octaves = 4
-perlin.fractal.lacunarity = 2.1
-perlin.fractal.gain = 0.45
-perlin.perturb.perturbType = fns.PerturbType.NoPerturb
+    def randomVals(self):
+        #self.perlin.frequency = 0.001
+        self.perlin.frequency = np.random.rand()*(0.005 - 0.001)+0.001
+        self.perlin.fractal.gain = np.random.rand()+2.1 #2.1-3
+        self.perlin.fractal.gain = 3 #2.1-3
 
-start = time.time()
-img = perlin.genAsGrid(shape)
-end = time.time()
-print(f"{end-start}")
-plt.imshow(img, cmap='gray')
-plt.show()
+    
+    def getImage(self):
+        img = self.perlin.genAsGrid(self.shape)
+        img = img/2 + 0.5
+        return img
+
+    def getGoals(self):
+        return (np.random.randint(0,1028,(1,2)), np.random.randint(0,1028,(1,2)))
+if __name__ =="__main__":
+    noise = Perlin()
+    plt.imshow(noise.getImage(),cmap="gray")
+    x,y = noise.getGoals()
+    plt.scatter(x=x, y=y, c='r', s=40)
+    plt.show()
