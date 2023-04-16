b28cc2dd8a62d9736d42a7968a6a129e24e2b455
Elijah <elijahmtcastle@gmail.com>
2023-04-16 13:17:58 -0600

    fixed perlin frequency it was to low for the new size

--- perlin.py
+++ perlin.py
@@ -8,7 +8,7 @@ class Perlin():
     N_threads = 4#TODO this can be changed to match how mnay cores your cpu has
     def __init__(self):
         self.perlin = fns.Noise(seed=self.seed, numWorkers=self.N_threads)
-        self.perlin.frequency = 0.005
+        self.perlin.frequency = 0.05
         self.perlin.noiseType = fns.NoiseType.Perlin
         self.perlin.fractal.octaves = 8
         self.perlin.fractal.lacunarity = 1.0#does not seem to be implemented
@@ -16,7 +16,7 @@ class Perlin():
 
     def randomVals(self):
         #self.perlin.frequency = 0.001
-        self.perlin.frequency = np.random.rand()*(0.005 - 0.001)+0.001
+        self.perlin.frequency = np.random.rand()*(0.05 -0.01)+0.01
         self.perlin.fractal.gain = np.random.rand()+2.1 #2.1-3
     
     def getImage(self):
@@ -32,5 +32,8 @@ if __name__ =="__main__":
     noise = Perlin()
     plt.imshow(noise.getImage(),cmap="gray")
     x,y = noise.getGoals()
-    plt.scatter(x=x, y=y, c='r', s=40)
-    plt.show()
+    while True:
+        plt.imshow(noise.getImage(),cmap="gray")
+        print(f"Frequency: {noise.perlin.frequency} Gain: {noise.perlin.fractal.gain}")
+        plt.scatter(x=x, y=y, c='r', s=40)
+        plt.show()
